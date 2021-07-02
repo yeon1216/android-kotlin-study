4bf3f1792e607125b149469a68093ca5f8ab412f
yeon1216 <sykim8770@gmail.com>
2021-07-02 11:00:04 +0900

    전자액자 프로젝트 진행중 (UI 작성)

--- ElectronicPictureFrames/app/src/main/java/com/example/electronicpictureframes/MainActivity.kt
+++ ElectronicPictureFrames/app/src/main/java/com/example/electronicpictureframes/MainActivity.kt
@@ -1,9 +1,34 @@
 package com.example.electronicpictureframes
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private val addPhotoButton: Button by lazy {
+        findViewById<Button>(R.id.addPhotoButton)
+    }
+
+    private val startPhotoFrameModeButton: Button by lazy {
+        findViewById<Button>(R.id.startPhotoFrameModeButton)
+    }
+
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.imageView11))
+            add(findViewById(R.id.imageView12))
+            add(findViewById(R.id.imageView13))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+        }
+    }
+
+    private val imageUriList: MutableList<Uri> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
