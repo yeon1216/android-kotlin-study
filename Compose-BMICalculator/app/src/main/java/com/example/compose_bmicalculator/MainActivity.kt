21d800311126a00e50a23f1cce1585c9f479ca5f
yeon1216 <sykim8770@gmail.com>
2022-03-10 16:43:15 +0900

    add home-screen in compose-bmi

--- Compose-BMICalculator/app/src/main/java/com/example/compose_bmicalculator/MainActivity.kt
+++ Compose-BMICalculator/app/src/main/java/com/example/compose_bmicalculator/MainActivity.kt
@@ -3,36 +3,78 @@ package com.example.compose_bmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.compose_bmicalculator.ui.theme.ComposeBMICalculatorTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBMICalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            HomeScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeScreen() {
+
+    val height = remember {
+        mutableStateOf("")
+    }
+    val (weight, setWeight) = remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("비만도 계산기")
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = height.value,
+                onValueChange = { height.value = it },
+                label = { Text("키")},
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            OutlinedTextField(
+                value = weight,
+                onValueChange = setWeight,
+                label = { Text("몸무게")},
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.align(Alignment.End)
+
+            ) {
+                Text("결과")
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeBMICalculatorTheme {
-        Greeting("Android")
-    }
+
 }
