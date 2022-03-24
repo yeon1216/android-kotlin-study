5b93df3979d92920f7e188e82453037250a4b53f
yeon1216 <sykim8770@gmail.com>
2022-03-24 17:42:34 +0900

    add compose-todolist project

---
+++ Compose-Todolist/app/src/main/java/com/example/compose_todolist/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.compose_todolist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_todolist.ui.theme.ComposeTodolistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
